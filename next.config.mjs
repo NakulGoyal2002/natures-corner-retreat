f4aa9fee404638abcf06d0a345c7c84f3d89e314
Nakul Goyal <nakul.21239@knit.ac.in>
2025-04-04 22:14:07 +0530

    minor changes

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
 		remotePatterns: [
 			{
 				protocol: "https",
-				hostname: "krzdxeytdqfdgyigspcd.supabase.co",
+				hostname: "jogepikrohdtulvavgri.supabase.co",
 				port: "",
 				pathname: "/storage/v1/object/public/cabin-images/**",
 			},
