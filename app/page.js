a69f2b9dc84b8dd6170d2a935a5aa56857953934
Nakul Goyal <nakul.21239@knit.ac.in>
2025-04-06 16:47:49 +0530

    chatbot added

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import bg from "@/public/bg.png";
+import Script from "next/script";
 
 export default function Page() {
 	return (
@@ -26,6 +27,55 @@ export default function Page() {
 					Explore luxury cabins
 				</Link>
 			</div>
+			{/* <div>
+				<script
+					defer
+					src='https://app.fastbots.ai/embed.js'
+					data-bot-id='cm95gqga111tdrik5z1q7vq4u'
+				></script>
+				<iframe
+					style={{ width: "400px", height: "600px" }}
+					src='https://app.fastbots.ai/embed/cm95gqga111tdrik5z1q7vq4u'
+				></iframe>
+			</div> */}
+			{/* <div className='flex justify-center items-center h-screen'>
+				<iframe
+					src='https://app.fastbots.ai/embed/cm95gqga111tdrik5z1q7vq4u'
+					style={{
+						width: "400px",
+						height: "600px",
+						border: "none",
+					}}
+					allow='clipboard-write'
+				></iframe>
+			</div> */}
+			<div style={floatingChatStyle}>
+				{/* <script
+					src='//code.tidio.co/jo0czodr7vp3gyivxuvzc6utzsqaecbx.js'
+					async
+				></script> */}
+				<script
+					defer
+					src='https://app.fastbots.ai/embed.js'
+					data-bot-id='cm95gqga111tdrik5z1q7vq4u'
+				></script>
+				{/* <iframe
+					src='https://app.fastbots.ai/embed/cm95gqga111tdrik5z1q7vq4u'
+					style={{
+						width: "350px",
+						height: "500px",
+						border: "none",
+						borderRadius: "12px",
+					}}
+					allow='clipboard-write'
+				/> */}
+			</div>
 		</main>
 	);
 }
+const floatingChatStyle = {
+	position: "fixed",
+	bottom: "20px",
+	right: "20px",
+	zIndex: 1000,
+};
